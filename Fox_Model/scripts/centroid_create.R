e48a2cff4ee531bae1aea9b31e04fc716e046e5b
scabecks <scabecks@gmail.com>
2017-12-21 17:15:39 +1000

    updated R script to create clustered points from fox data

--- Fox_Model/scripts/centroid_create.R
+++ Fox_Model/scripts/centroid_create.R
@@ -1,8 +1,13 @@
 # Aggregates data points with 'n' distance as centroids
 # To be used to aggregate data values within aggregation distnce
+library(sf)
+library(rgeos)
+library(geosphere)
+library(sp)
 
-foxes_so <- as(foxes,"Sparial") # convert sf to sp
-mist<-distm(spTransform(foxes_sp,4326)) # requires in 4326 / lat-long
+foxes <- read_sf("Fox_Model/foxes_points.gpkg")
+foxes_sp <- as(foxes,"Spatial") # convert sf to sp
+mdist <- distm(spTransform(foxes_sp,CRS("+init=epsg:4326"))) # requires in 4326 / lat-long
 hc<-hclust(as.dist(mdist),method = 'complete') # cluster by distance
 foxes_sp$clust <- cutree(hc, h = 1000) # 1km disrance
 
@@ -17,6 +22,8 @@ for (i in 1:max(foxes_sp$clust)){
 cent <- as.data.frame(cent)
 names(cent) <- c("x","y")
 
-cent_sf<- st_as_sf(cent,coords=c("x","y"), crs = 3577)
+# convert to sf object
+cent_sf <- st_as_sf(cent,coords=c("x","y"), crs = 3577)
 
-st_write(cent_sf,'/home/scottca/Desktop/test.gpkg')
+# save to geopackage
+st_write(cent_sf,'Fox_Model/foxes_cluster_points.gpkg')
