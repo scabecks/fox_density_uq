3ee23e43310656a013bd234b823ff94d4d748bd4
scabecks <scabecks@gmail.com>
2020-07-10 18:16:36 +1000

    Added better descriptions of functions.

--- fox_model/python_scripts/fox_points.py
+++ fox_model/python_scripts/fox_points.py
@@ -11,6 +11,23 @@ import rasterio as rio
 from time import strptime
 
 def precip_6_month(date):
+    """
+    Parameters
+    ----------
+    date : str
+        The date needs to be provided as a string in a format 'YYYY-MM'.
+        This is a concatination of year and month attributes from the fox
+        density database, and allows for easy indexing to Worldclim
+        precipitation data.
+
+    Returns
+    -------
+    Geotiff of summed Worldclim precipitation rasters for the period prior
+    to the fox density data point.
+
+    """
+    assert len(date) == 7, "Date needs to be in YYYY-MM format."
+    assert r_list not in locals(), "Directory of rasters not defined."
     idx = r_list.index('wc2.1_2.5m_prec_' + date + '.tif')
     r0 = rio.open('worldclim/' + r_list[idx])#.read(1)
     profile = r0.profile
@@ -33,7 +50,11 @@ def precip_6_month(date):
         dst.write(rs, 1)
         
          
-#Read in foxes data
+"""
+Read in foxes data:
+    Data is cleaned by removing records that do not have valid collection dates
+    or spatial information.
+"""
 foxes = pd.read_csv('fox_densities_march2018-enso.csv')
 foxes = foxes[foxes['month'].notna()]
 foxes['month_int'] = foxes['month'].apply(lambda x: f'{strptime(x, "%b").tm_mon:02d}') 
